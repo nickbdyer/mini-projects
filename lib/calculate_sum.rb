ab765f61450bb48d2790a908c8e75a32a1dc841a
Nick Dyer <nbdyer@gmail.com>
2015-03-04 18:31:00 +0000

    Doubles text file size, includes code for normalising length of bignums

--- lib/calculate_sum.rb
+++ lib/calculate_sum.rb
@@ -1,7 +1,9 @@
 array = []
 text = File.open('./lib/largenumbers.txt', 'r')
 text.each_line do |line|
-  array << line.slice(0..12).to_i
+  array << line.slice(0..42).to_i
 end
 
 puts array.inject(:+)
+
+(sprintf '%050d', 1).slice(0..10)
